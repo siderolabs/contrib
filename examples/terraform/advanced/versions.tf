b14dd77d63e72aa3b72c354ff179583b8fb36cde
Noel Georgi <git@frezbo.dev>
2024-09-16 13:47:48 +0530

    chore: bump tf providers
    Bump TF providers.
    
    Signed-off-by: Noel Georgi <git@frezbo.dev>

--- examples/terraform/advanced/versions.tf
+++ examples/terraform/advanced/versions.tf
@@ -14,7 +14,7 @@ terraform {
     }
     talos = {
       source  = "siderolabs/talos"
-      version = "0.6.0-alpha.1"
+      version = "0.6.0-beta.0"
     }
   }
 }
--- examples/terraform/basic/versions.tf
+++ examples/terraform/basic/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     talos = {
       source  = "siderolabs/talos"
-      version = "0.6.0-alpha.1"
+      version = "0.6.0-beta.0"
     }
   }
 }
